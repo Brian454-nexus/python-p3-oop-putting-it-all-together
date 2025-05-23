1bef10c29d4630a47aded18879e5601ee2a355a0
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-05-23 12:23:23 +0300

    Put together oop lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
+# Define the Book class
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        # Set the title of the book
+        self.title = title
+        # Set the page count of the book (uses the property setter)
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        # Getter for the page_count attribute
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        # Setter for the page_count attribute, ensures value is an integer
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        # Print a message indicating the page has been turned
+        print("Flipping the page...wow, you read fast!")
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+# Define the Shoe class
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        # Set the brand of the shoe
+        self.brand = brand
+        # Set the size of the shoe (uses the property setter)
+        self.size = size
+        # Initialize the condition attribute to None
+        self.condition = None
+
+    @property
+    def size(self):
+        # Getter for the size attribute
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        # Setter for the size attribute, ensures value is an integer
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        # Print a message indicating the shoe has been repaired
+        print("Your shoe is as good as new!")
+        # Set the condition attribute to 'New' after repair
+        self.condition = "New"
